08837cd93ff3c54b8f68a52f54dbfd5bef796091
EmadDaraghmeh <s12116485@stu.najah.edu>
2024-05-16 13:28:22 +0300

    the button in the ColoratedButton.js was updated

--- client/src/Components/loadingInfluencerPage/ColoratedButtons.js
+++ client/src/Components/loadingInfluencerPage/ColoratedButtons.js
@@ -4,16 +4,16 @@ const ColoratedButtons = () => {
   
     return (
         <div class="buttons-container">
-            <button name="action1" id='blue' >Beauty</button>
-            <button name="action2"  id='green'>Fashion</button>
-            <button name="action3" id='orang'>Sports</button>
-            <button name="action4" id='purple'>Technology </button>
-            <button name="action5" id='red'>Healthcare</button>
-            <button name="action5" id='yellow'>Food</button>
-            <button name="action5" id='aqua'>Travel</button>
-            <button name="action5" id='pink'>Entertainment</button>
-            <button name="action5" id='dark-green'>Educatoin</button>
-            <button name="action5" id='brown'>Lifestyle</button>
+            <button name="Beauty"  className='brand-button' id='blue' >Beauty</button>
+            <button name="Fashion" className='brand-button' id='green'>Fashion</button>
+            <button name="Sports"  className='brand-button' id='orang'>Sports</button>
+            <button name="Technology" className='brand-button' id='purple'>Technology </button>
+            <button name="Healthcare"  className='brand-button' id='red'>Healthcare</button>
+            <button name="Food" className='brand-button' id='yellow'>Food</button>
+            <button name="Travel"  className='brand-button' id='aqua'>Travel</button>
+            <button name="Entertainment"  className='brand-button' id='pink'>Entertainment</button>
+            <button name="Educatoin"  className='brand-button' id='dark-green'>Educatoin</button>
+            <button name="Lifestyle"  className='brand-button' id='brown'>Lifestyle</button>
         </div>
   )
 }
