286ebedbc109f02b4830c6aba7e7a581d4119a20
leen_hasoun <leenhasoun04@gmail.com>
2024-05-18 20:46:31 +0300

    singup popup

---
+++ client/src/Components/Signuppop/Signuppop.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Signuppop.css';
+
+function Signuppop() {
+  return (
+   
+   <div className="modal-dialog modal-dialog-scrollable">
+
+</div>
+
+    
+  );
+}
+
+//export default Signuppop;
